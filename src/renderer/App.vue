0eeb55df6e5bae16a9bcd02db43ef432976aad98
xinhao <932357832@qq.com>
2019-04-02 18:13:41 +0800

    feat: 移除 vue app 入口的鼠标监听事件

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -5,25 +5,7 @@
 </template>
 
 <script>
-  const ipc = window.require('electron').ipcRenderer
-  export default {
-    name: 'stocktools',
-    mounted () {
-      // 单独为页面做事件监听可以防止出现部分处于 body 中的 element ui 元素监听不到的问题
-      document.addEventListener('mouseenter', this.mouseEnter)
-      document.addEventListener('mouseleave', this.mouseLeave)
-    },
-    methods: {
-      // 鼠标进入
-      mouseEnter () {
-        ipc.send('mouseenter')
-      },
-      // 鼠标移出
-      mouseLeave () {
-        ipc.send('mouseleave')
-      }
-    }
-  }
+  export default {}
 </script>
 
 <style>
