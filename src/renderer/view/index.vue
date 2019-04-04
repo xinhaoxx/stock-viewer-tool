294b76a9d4b164dd94398bbe8fe6cc58eb044a5a
xinhao <932357832@qq.com>
2019-04-04 17:12:16 +0800

    feat: 优化主窗口代码，优化注释描述

--- src/renderer/view/index.vue
+++ src/renderer/view/index.vue
@@ -64,7 +64,7 @@
           <template slot-scope="props">
             <p class="stock-volume">
               {{transVolume(props.row.volume)}}{{props.row.code.indexOf('hk')>-1 ?
-              '万股':(props.row.volume.length > 4 ? '万手':'手')}}
+              '股':'手'}}
             </p>
           </template>
         </el-table-column>
@@ -88,23 +88,20 @@
         </el-table-column>
       </el-table>
     </div>
+    <!--加自选弹窗-->
     <optional-dialog ref="optionalDialog" @finish="fetchData"/>
   </div>
 </template>
 
 <script>
-  import Sortable from 'sortablejs' // 拖拽库
+  import {apiUrl, timeSpan, stockIndex} from '../libs/constant' // api,请求间隔,三大指数
+  import Sortable from 'sortablejs' // 表格拖拽库
   import OptionalDialog from '../components/optional' // 添加功能组件
-
-  const drag = require('electron-drag') // 窗体移动库
-  const Mousetrap = require('mousetrap') // 键位映射库
 
   const shell = require('electron').shell
   const ipc = window.require('electron').ipcRenderer
-
-  const stocksIndex = ['sh000001', 'sz399001', 'sz399006'] // 指数
-
-  const apiUrl = 'http://qt.gtimg.cn/' // 接口地址
+  const Mousetrap = require('mousetrap') // 键位映射库
+  const drag = require('electron-drag') // 窗体移动库
 
   export default {
     components: {
@@ -112,41 +109,27 @@
     },
     data () {
       return {
-        indexs: [],
-        optionals: []
+        indexs: [], // 指数部分
+        optionals: [] // 自选部分
       }
     },
     created () {
       this.fetchData()
     },
     mounted () {
-      // 轮训获取数据
-      setInterval(this.fetchData, 5000)
-      // 拖拽功能
-      this.rowDrop()
-      Mousetrap.bind(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'], (e) => {
-        this.$refs['optionalDialog'].show(e.key)
-      })
-      Mousetrap.bind(['command+k', 'ctrl+k'], function () {
-        ipc.send('open-devtools')
-      })
-      // 使用 electron-drag 解决顶栏无法捕获鼠标事件的问题（暂不支持linux）
-      drag('.header')
-      // 如果不支持则使用样式的方式
-      if (!drag.supported) {
-        document.querySelector('.header').style['-webkit-app-region'] = 'drag'
-      }
-
-      this.initIpcListener()
-
-      // 单独为页面做事件监听可以防止出现部分处于 body 中的 element ui 元素监听不到的问题
-      document.addEventListener('mouseenter', this.mouseEnter)
-      document.addEventListener('mouseleave', this.mouseLeave)
+      setInterval(this.fetchData, timeSpan)// 轮训获取数据
+      this.rowDrop() // 拖拽功能
+      this.bindKey() // 键盘映射
+      this.bindMouse() // 鼠标移动
+      this.bindDrag() // 绑定顶栏拖拽
+      this.initIpcListener() // 初始化主窗口监听事件
     },
     methods: {
-      // 获取自选股数据
+      /**
+       * 通过腾讯接口获取指数&自选股的数据
+       */
       fetchData () {
-        let index = stocksIndex.map(item => 's_' + item) // 接口需要加前缀
+        let index = stockIndex.map(item => 's_' + item) // 接口需要加前缀
         let storage = localStorage.getItem('optionals')
         let storageOptional = []
         if (storage !== '' && storage !== null) {
@@ -159,7 +142,10 @@
           this.resolveData(res.data)
         })
       },
-      // 解析内容
+      /**
+       * 解析所有获取的股票数据
+       * @param data {string} - 股票数据字符串
+       */
       resolveData (data) {
         this.indexs = []
         this.optionals = []
@@ -168,10 +154,8 @@
         let splitData = data.split(';')
         splitData.forEach((item, index) => {
           let splitItem = item.split('=') // 按照等于号截取前后部分
-
           let prefix = splitItem[0] // 前缀部分 // 等号前
           let suffix = splitItem[1].match(/"(.*?)"/)[1] // 后缀内容，只取双引号内容
-
           let content = suffix.substring(0, suffix.length - 1).split('~')
           let stock = {
             code: prefix.substring(4, 12),
@@ -184,7 +168,7 @@
             volume: content[6],
             status: content[8] === '' ? null : content[8]
           }
-          if (index < stocksIndex.length) {
+          if (index < stockIndex.length) {
             this.indexs.push(stock)
           } else {
             this.optionals.push(stock)
@@ -192,26 +176,17 @@
         })
         this.updateToolTips(this.optionals)
       },
-      showDetail (row) {
-        ipc.send('create', row.code)
-      },
-      // 关闭窗口
-      setWinClose () {
-        ipc.send('main-window-close')
-      },
-      // 最小化窗口
-      setWinMin () {
-        ipc.send('main-window-min')
-      },
-      // 添加自选
+
+      /**
+       * 弹窗添加自选股
+       */
       addOptional () {
         this.$refs['optionalDialog'].show()
       },
-      // 显示右键菜单
-      showContext (row) {
-        ipc.send('main-right-click', row.code)
-      },
-      // 删除自选
+      /**
+       * 删除自选
+       * @param code {string} - 个股代码
+       */
       deleteStock (code) {
         let storage = localStorage.getItem('optionals')
         let storageOptional = storage.split(',')
@@ -229,26 +204,56 @@
           duration: 2000
         })
       },
-      // 更新托盘提示
-      updateToolTips (data) {
-        let content = data.map(item => {
-          return `${item.name}：当前价 ¥${item.price},涨跌 ¥${item.gain.price}（${item.gain.percent}%）`
+      /**
+       * 执行修改数据
+       */
+      modifyData () {
+        const modified = this.optionals.map((item) => item.code)
+        localStorage.setItem('optionals', modified.join(','))
+      },
+      /**
+       * 显示单支股票的详情，通过新窗口显示
+       * @param row {object} - 个股数据
+       */
+      showDetail (row) {
+        ipc.send('create', row.code)
+      },
+      /**
+       * 自选股的行拖拽排序功能
+       */
+      rowDrop () {
+        const tbody = document.querySelector('.el-table__body-wrapper tbody')
+        const _this = this
+        Sortable.create(tbody, {
+          animation: 120,
+          onEnd ({newIndex, oldIndex}) {
+            const currRow = _this.optionals.splice(oldIndex, 1)[0]
+            _this.optionals.splice(newIndex, 0, currRow)
+            _this.modifyData()
+          }
         })
-        ipc.send('main-tray-update', content.join('\n'))
       },
 
-      // 将金额转为万为单位
-      transVolume (number) {
-        number = parseInt(number) + ''
-        if (number.length > 4) {
-          let integer = number.substring(0, number.length - 4)
-          let decimal = integer.length > 3 ? '' : ('.' + number.substring(number.length - 5, number.length - 3))
-          return integer + decimal
-        } else {
-          return number
-        }
+      /**
+       * 点击跳转雪球网站
+       * @param code {string} - 个股代码
+       */
+      openInXueQiu (code) {
+        let marketPrefix = code.substring(0, 2).toLowerCase()
+        shell.openExternal('https://xueqiu.com/S/' + (marketPrefix === 'hk' ? code.substring(2) : code))
       },
-      // 主线程事件监听
+      /**
+       * 点击跳转东方财富网站
+       * * @param code {string} - 个股代码
+       */
+      openInGuBa (code) {
+        let marketPrefix = code.substring(0, 2).toLowerCase()
+        shell.openExternal('http://guba.eastmoney.com/list,' + (marketPrefix === 'hk' ? code : (code === 'sh000001' ? 'szzs' : code.substring(2))) + '.html')
+      },
+
+      /**
+       * 主窗口线程事件监听
+       */
       initIpcListener () {
         ipc.on('show-xueqiu', (event, code) => {
           this.openInXueQiu(code)
@@ -269,50 +274,76 @@
           this.deleteStock(code)
         })
       },
-      // 点击跳转雪球网站
-      openInXueQiu (code) {
-        let marketPrefix = code.substring(0, 2).toLowerCase()
-        shell.openExternal('https://xueqiu.com/S/' + (marketPrefix === 'hk' ? code.substring(2) : code))
+      /**
+       * 关闭主窗口
+       */
+      setWinClose () {
+        ipc.send('main-window-close')
       },
-      // 点击跳转东方财富网站
-      openInGuBa (code) {
-        let marketPrefix = code.substring(0, 2).toLowerCase()
-        shell.openExternal('http://guba.eastmoney.com/list,' + (marketPrefix === 'hk' ? code : (code === 'sh000001' ? 'szzs' : code.substring(2))) + '.html')
+      /**
+       * 最小化主窗口
+       */
+      setWinMin () {
+        ipc.send('main-window-min')
       },
-      // 行拖拽
-      rowDrop () {
-        const tbody = document.querySelector('.el-table__body-wrapper tbody')
-        const _this = this
-        Sortable.create(tbody, {
-          animation: 120,
-          onEnd ({newIndex, oldIndex}) {
-            const currRow = _this.optionals.splice(oldIndex, 1)[0]
-            _this.optionals.splice(newIndex, 0, currRow)
-            _this.modifyData()
-          }
+      /**
+       * 更新系统托盘的提示文字，显示当前前几条个股的信息
+       * @param data {array} - 所有自选股的数据
+       * @description windows下只能显示部分文字，会溢出，待解决
+       */
+      updateToolTips (data) {
+        let content = data.map(item => {
+          return `${item.name}：当前价 ¥${item.price},涨跌 ¥${item.gain.price}（${item.gain.percent}%）`
+        })
+        ipc.send('main-tray-update', content.join('\n'))
+      },
+      /**
+       * 右键指数&个股显示上下文菜单
+       * @param row {object} - 个股数据
+       */
+      showContext (row) {
+        ipc.send('main-right-click', row.code)
+      },
+
+      /**
+       * 绑定键盘映射
+       */
+      bindKey () {
+        Mousetrap.bind(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'], (e) => {
+          this.$refs['optionalDialog'].show(e.key)
+        })
+        Mousetrap.bind(['command+k', 'ctrl+k'], function () {
+          ipc.send('open-devtools')
         })
       },
-      // 修改数据
-      modifyData () {
-        const modified = this.optionals.map((item) => item.code)
-        localStorage.setItem('optionals', modified.join(','))
+      /**
+       * 使用 electron-drag 解决顶栏无法捕获鼠标事件的问题（暂不支持linux）
+       */
+      bindDrag () {
+        drag('.header')
+        // 如果不支持则使用样式的方式
+        if (!drag.supported) {
+          document.querySelector('.header').style['-webkit-app-region'] = 'drag'
+        }
       },
-      // 鼠标进入
+      /**
+       * 单独为页面做事件监听可以防止出现部分处于 body 中的 element ui 元素监听不到的问题
+       */
+      bindMouse () {
+        document.addEventListener('mouseenter', this.mouseEnter)
+        document.addEventListener('mouseleave', this.mouseLeave)
+      },
+      /**
+       * 鼠标进入主窗口的情况
+       */
       mouseEnter () {
         ipc.send('main-mouse-enter')
       },
-      // 鼠标移出
+      /**
+       * 鼠标移出主窗口的情况
+       */
       mouseLeave () {
         ipc.send('main-mouse-leave')
-      },
-      /**
-       * 比较两个值
-       * @param value 被比较值
-       * @param compare 比较值
-       * @returns {string} 类名
-       */
-      comparePrice (value, compare) {
-        return value > compare ? 'gain-more' : (value < compare ? 'gain-less' : '')
       }
     }
   }
